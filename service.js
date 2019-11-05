c02795d5dce4112332002b42142d55c38f92a6fe
gustavogaspar <gaspar.gualves@gmail.com>
2019-11-05 22:13:31 +0000

    final build

--- service.js
+++ service.js
@@ -1,49 +1,51 @@
 const OracleBot = require('@oracle/bots-node-sdk');
 const { WebhookClient, WebhookEvent } = OracleBot.Middleware;
-const io = require('socket.io');
+var messageFromBot
 
 module.exports = (app) => {
-  const logger = console;
+    const logger = console;
 
-  OracleBot.init(app, {
-    logger,
-  });
-
-io.on('connection', (socket) => {
-    console.log('User connected')
-})
-
-  const webhook = new WebhookClient({
-    channel: {
-      url: 'https://botv2iad1I0314HC40A97bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/2e99cc61-b856-48f3-a2d6-9b265fb0d868' ,
-      secret: '71doE4zWl74v87E6mzHfeYo4asGvgFV4' ,
-    }
-  });
-
-  webhook
-    .on(WebhookEvent.ERROR, err => logger.error('Error:', err.message))
-    .on(WebhookEvent.MESSAGE_SENT, message => logger.info('Message to bot:', message))
-    .on(WebhookEvent.MESSAGE_RECEIVED, message => {
-      logger.info('Message from bot:', message);
+    OracleBot.init(app, {
+        logger,
     });
 
-  app.post('/bot/message', webhook.receiver());
+    const webhook = new WebhookClient({
+        channel: {
+            url: 'https://botv2iad1I0314HC40A97bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/2e99cc61-b856-48f3-a2d6-9b265fb0d868',
+            secret: 'pwhFbDblYznsyhfboBFKZZTSuS2aF5Al',
+        }
+    });
+
+    webhook
+        .on(WebhookEvent.ERROR, err => logger.error('Error:', err.message))
+        .on(WebhookEvent.MESSAGE_SENT, message => logger.info('Message to bot:', message))
+        .on(WebhookEvent.MESSAGE_RECEIVED, message => {
+            logger.info('Message from bot:', message);
+            messageFromBot = message
+        });
+
+    app.post('/bot/message', webhook.receiver());
 
 
-  app.post('/test/message', (req, res) => {
-    const { user, text } = req.body;
-
-    const MessageModel = webhook.MessageModel();
-    const message = {
-      userId: user,
-      messagePayload: MessageModel.textConversationMessage(text)
-    };
-
-    webhook.send(message)
-      .then(async () =>  {
-          await webhook.on(WebhookEvent.MESSAGE_RECEIVED, mes => {
-              res.send(mes.messagePayload.text)
-          })
-          }, e => res.status(400).end(e.message));
-  });
+    app.post('/test/message', (req, res) => {
+        const { user, text } = req.body;
+        // construct message to bot from the client message format
+        const MessageModel = webhook.MessageModel();
+        const message = {
+            userId: user,
+            messagePayload: MessageModel.textConversationMessage(text)
+        };
+        // send to bot webhook channel
+        webhook.send(message)
+            .then(() => {
+                webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
+                    logger.info('Message from bot:', message);
+                    message
+                });
+                setTimeout(() => {
+                    console.log('messageFromBot', messageFromBot)
+                    res.send(messageFromBot)
+                }, 1500);
+            }, e => res.status(400).end(e.message));
+    });
 }
